89de4b9652967326207feb4c53e01078ab1ef0ce
CarlosMito24 <carlosalfonsomito@gmail.com>
2023-08-11 11:28:23 -0600

    Agregar archivos de proyecto.

---
+++ ToDo4030360/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using ToDo4030360.Views;
+
+namespace ToDo4030360;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+		Routing.RegisterRoute(nameof(TodoItemPage), typeof(TodoItemPage));
+	}
+}
---
+++ ToDo4030360/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using ToDo4030360.Views;
+using ToDo4030360.Data;
+using Microsoft.Extensions.Logging;
+
+namespace ToDo4030360;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+		builder.Services.AddSingleton<TodoListPage>();
+		builder.Services.AddTransient<TodoItemPage>();
+		builder.Services.AddSingleton<TodoItemDatabase>();
+
+		return builder.Build();
+	}
+}
